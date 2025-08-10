bf33f7a5768a041828b185fcd5778586c399ea1b
tianxin1024 <tianxin4201@163.com>
2025-08-10 21:12:47 +0800

    add tmux

--- env.sh
+++ env.sh
@@ -1,78 +1,44 @@
 #!/bin/bash
 
-HIKTOOLS="/tools"
+HIKTOOLS="$(pwd)"
 
-
-# # set a fancy prompt (non-color, unless we know we "want" color)
-# case "$TERM" in
-# xterm-color|*-256color) color_prompt=yes;;
-# esac
-
-# # uncomment for a colored prompt, if the terminal has the capability; turned
-# # off by default to not distract the user: the focus in a terminal window
-# # should be on the output of commands, not on the prompt
-# #force_color_prompt=yes
-
-# if [ -n "$force_color_prompt" ]; then
-# if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
-# # We have color support; assume it's compliant with Ecma-48
-# # (ISO/IEC-6429). (Lack of such support is extremely rare, and such
-# # a case would tend to support setf rather than setaf.)
-# color_prompt=yes
-# else
-# color_prompt=
-# fi
-# fi
-
-# if [ "$color_prompt" = yes ]; then
-# PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
-# else
-# PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
-# fi
-# unset color_prompt force_color_prompt
-
-# # If this is an xterm set the title to user@host:dir
-# case "$TERM" in
-# xterm*|rxvt*)
-# PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
-# ;;
-# *)
-# ;;
-# esac
-
-# # enable color support of ls and also add handy aliases
-# if [ -x /usr/bin/dircolors ]; then
-# test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
-# alias ls='ls --color=auto'
-# #alias dir='dir --color=auto'
-# #alias vdir='vdir --color=auto'
-
-# alias grep='grep --color=auto'
-# alias fgrep='fgrep --color=auto'
-# alias egrep='egrep --color=auto'
-# fi
-
-# export LANG=en_US.UTF-8
-# export LC_ALL=en_US.UTF-8
-# export TERM=xterm-256color
-
+## =============================================================== ##
+##   neovim
+## =============================================================== ##
 export NVIM_HOME="$HIKTOOLS/nvim-linux64"
 export PATH="$NVIM_HOME/bin:$PATH"
 
-# >>> alias config >>>
-alias vim='/tools/lvim/lvim'
-export EDITOR=lvim
+## =============================================================== ##
+##   lunarvim
+## =============================================================== ##
+# alias vim='${HIKTOOLS}/lvim/lvim'
+# export EDITOR=lvim
 
-export AIDER_EDITOR=lvim
-export AIDER_DARK_MODE=true
+# export AIDER_EDITOR=lvim
+# export AIDER_DARK_MODE=true
 
 
+## =============================================================== ##
+##   tmux config 
+## =============================================================== ##
 export TMUX_HOME="$HIKTOOLS/tmux"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$TMUX_HOME/lib"
 export PATH="$TMUX_HOME/bin:$PATH"
 
-cp -r $HIKTOOLS/config/.tmux ~/.tmux
-cp $HIKTOOLS/config/.tmux.conf ~/.tmux.conf
+tmux_dir="$HOME/.tmux"
+tmux_conf="$HOME/.tmux.conf"
+
+# 检查 .tmux 目录是否存在
+if [[ ! -d "$tmux_dir" ]]; then
+    echo "Copying .tmux directory..."
+    cp -r "$HIKTOOLS/config/.tmux" "$tmux_dir"
+fi
+
+# 检查 .tmux.conf 是否存在
+if [[ ! -f "$tmux_conf" ]]; then
+    echo "Copying .tmux.conf..."
+    cp "$HIKTOOLS/config/.tmux.conf" "$tmux_conf"
+fi
 
 alias tnew="tmux new -s"
 alias tat="tmux attach"
